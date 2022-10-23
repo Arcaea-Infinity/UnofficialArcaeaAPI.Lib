bcfd1b7aa6a67ffce62806c93d0178d73e0932f2
b1acksoil <dev@sorabs.cc>
2022-10-23 23:52:12 +0800

    feat(assets): add preview api

--- ArcaeaUnlimitedAPI.Lib/Core/AuaAssetsApi.cs
+++ ArcaeaUnlimitedAPI.Lib/Core/AuaAssetsApi.cs
@@ -98,5 +98,46 @@ public class AuaAssetsApi
     public Task<byte[]> Song(string songname, ArcaeaDifficulty difficulty)
         => GetSong(songname, AuaSongQueryType.SongName, difficulty);
 
-    #endregion /assets/icon
+    #endregion /assets/song
+    
+    #region /assets/preview
+
+    private async Task<byte[]> GetPreview(string songname, AuaSongQueryType queryType,
+        ArcaeaDifficulty difficulty)
+    {
+        var qb = new QueryBuilder();
+
+        qb.Add(queryType == AuaSongQueryType.SongId ? "songid" : "songname", songname);
+
+        if (queryType != AuaSongQueryType.FileName)
+            qb.Add("difficulty", ((int)difficulty).ToString());
+
+        return await _client.GetByteArrayAsync("assets/preview" + qb.Build());
+    }
+
+    /// <summary>
+    /// Get chart preview.
+    /// </summary>
+    /// <endpoint>/assets/preview</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying, or sid in Arcaea songlist.</param>
+    /// <param name="queryType">Specify the query type between songname and songid</param>
+    /// <param name="difficulty">Song difficulty</param>
+    /// <returns>Byte array represents the image</returns>
+    /// <remarks>This API is not available for release version of AUA. It is provided by AffPreview.</remarks>
+    public Task<byte[]> Preview(string songname, AuaSongQueryType queryType = AuaSongQueryType.SongName,
+        ArcaeaDifficulty difficulty = ArcaeaDifficulty.Future)
+        => GetPreview(songname, queryType, difficulty);
+
+    /// <summary>
+    /// Get chart preview.
+    /// </summary>
+    /// <endpoint>/assets/preview</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying</param>
+    /// <param name="difficulty">Song difficulty</param>
+    /// <returns>Byte array represents the image</returns>
+    /// <remarks>This API is not available for release version of AUA. It is provided by AffPreview.</remarks>
+    public Task<byte[]> Preview(string songname, ArcaeaDifficulty difficulty)
+        => GetPreview(songname, AuaSongQueryType.SongName, difficulty);
+
+    #endregion /assets/preview
 }
