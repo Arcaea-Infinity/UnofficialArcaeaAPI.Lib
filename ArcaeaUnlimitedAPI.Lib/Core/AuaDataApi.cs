f94ee2bbac03f62e86640de27cdecde7ef01d46d
b1acksoil <dev@sorabs.cc>
2022-10-23 19:52:36 +0800

    refactor: move data apis

--- ArcaeaUnlimitedAPI.Lib/Core/AuaDataApi.cs
+++ ArcaeaUnlimitedAPI.Lib/Core/AuaDataApi.cs
@@ -1,142 +1,133 @@
-﻿using ArcaeaUnlimitedAPI.Lib.Core;
+﻿using System.Text.Json;
 using ArcaeaUnlimitedAPI.Lib.Models;
 using ArcaeaUnlimitedAPI.Lib.Responses;
+using ArcaeaUnlimitedAPI.Lib.Utils;
 
-namespace ArcaeaUnlimitedAPI.Lib;
+namespace ArcaeaUnlimitedAPI.Lib.Core;
 
-public class AuaClient
+public class AuaDataApi
 {
-    public HttpClient? HttpClient { get; set; }
+    private readonly HttpClient _client;
 
-    public string Token { get; set; } = "";
-
-    public string UserAgent { get; set; } = "";
-
-    public int Timeout { get; set; } = 60;
-
-    public string ApiUrl { get; set; } = "";
-
-    public AuaUserApi User { get; private set; } = null!;
-    public AuaSongApi Song { get; private set; } = null!;
-    public AuaAssetsApi Assets { get; private set; } = null!;
-    public AuaDataApi Data { get; private set; } = null!;
-
-    public AuaClient Initialize()
+    public AuaDataApi(HttpClient client)
     {
-        if (HttpClient is null)
-        {
-            var client = new HttpClient
-            {
-                BaseAddress = new Uri(ApiUrl.EndsWith("/") ? ApiUrl : ApiUrl + "/"),
-                Timeout = new TimeSpan(0, 0, 0, Timeout)
-            };
-
-            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Token);
-            client.DefaultRequestHeaders.Add("User-Agent", UserAgent); // For compatibility
-
-            HttpClient = client;
-        }
-
-        User = new AuaUserApi(HttpClient);
-        Song = new AuaSongApi(HttpClient);
-        Assets = new AuaAssetsApi(HttpClient);
-        Data = new AuaDataApi(HttpClient);
-
-        return this;
+        _client = client;
     }
     
-    #region Obsoleted
+    private async Task<AuaUpdateContent> GetUpdate()
+    {
+        var response = JsonSerializer.Deserialize<AuaResponse<AuaUpdateContent>>(
+            await _client.GetStringAsync("/data/update"))!;
+        if (response.Status < 0)
+            throw new AuaException(response.Status, response.Message!);
+        return response.Content!;
+    }
 
     /// <summary>
     /// Get latest Arcaea apk download url.
     /// </summary>
     /// <endpoint>/data/update</endpoint>
     /// <returns>Update content with url and version</returns>
-    [Obsolete("Use Data.Update instead.")]
-    public Task<AuaUpdateContent> Update() => Data.Update();
-
-
-    /// <summary>
-    /// Get global play data of a song.
-    /// </summary>
-    /// <endpoint>/data/playdata</endpoint>
-    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
-    /// <param name="queryType">Specify the query type between songname and songid</param>
-    /// <param name="difficulty">Song difficulty</param>
-    /// <param name="start">Range of potential start (100*)</param>
-    /// <param name="end">Range of potential end (100*)</param>
-    /// <returns>Play data content</returns>
-    [Obsolete("Use Data.Playdata instead.")]
-    public Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
-        ArcaeaDifficulty difficulty, int start, int end)
-        => Data.Playdata(songname, queryType, difficulty, start, end);
-
-    /// <summary>
-    /// Get global play data of a song.
-    /// </summary>
-    /// <endpoint>/data/playdata</endpoint>
-    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
-    /// <param name="queryType">Specify the query type between songname and songid</param>
-    /// <param name="difficulty">Song difficulty</param>
-    /// <param name="start">Range of potential start (100*)</param>
-    /// <param name="end">Range of potential end (100*)</param>
-    /// <returns>Play data content</returns>
-    [Obsolete("Use Data.Playdata instead.")]
-    public Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
-        ArcaeaDifficulty difficulty, double start, double end)
-        => Data.Playdata(songname, queryType, difficulty, start, end);
-
-    /// <summary>
-    /// Get global play data of a song.
-    /// </summary>
-    /// <endpoint>/data/playdata</endpoint>
-    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
-    /// <param name="queryType">Specify the query type between songname and songid</param>
-    /// <param name="start">Range of potential start (100*)</param>
-    /// <param name="end">Range of potential end (100*)</param>
-    /// <returns>Play data content</returns>
-    [Obsolete("Use Data.Playdata instead.")]
-    public Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
-        int start, int end)
-        => Data.Playdata(songname, queryType, start, end);
-
-    /// <summary>
-    /// Get global play data of a song.
-    /// </summary>
-    /// <endpoint>/data/playdata</endpoint>
-    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
-    /// <param name="queryType">Specify the query type between songname and songid</param>
-    /// <param name="start">Range of potential start (100*)</param>
-    /// <param name="end">Range of potential end (100*)</param>
-    /// <returns>Play data content</returns>
-    [Obsolete("Use Data.Playdata instead.")]
-    public Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
-        double start, double end)
-        => Data.Playdata(songname, queryType, start, end);
-
-    /// <summary>
-    /// Get global play data of a song.
-    /// </summary>
-    /// <endpoint>/data/playdata</endpoint>
-    /// <param name="songname">Any song name for fuzzy querying</param>
-    /// <param name="start">Range of potential start (100*)</param>
-    /// <param name="end">Range of potential end (100*)</param>
-    /// <returns>Play data content</returns>
-    [Obsolete("Use Data.Playdata instead.")]
-    public Task<AuaPlaydataContent[]> Playdata(string songname, int start, int end)
-        => Data.Playdata(songname, start, end);
-
-    /// <summary>
-    /// Get global play data of a song.
-    /// </summary>
-    /// <endpoint>/data/playdata</endpoint>
-    /// <param name="songname">Any song name for fuzzy querying</param>
-    /// <param name="start">Range of potential start (100*)</param>
-    /// <param name="end">Range of potential end (100*)</param>
-    /// <returns>Play data content</returns>
-    [Obsolete("Use Data.Playdata instead.")]
-    public Task<AuaPlaydataContent[]> Playdata(string songname, double start, double end)
-        => Data.Playdata(songname, start, end);
+    public Task<AuaUpdateContent> Update() => GetUpdate();
     
-    #endregion Obsoleted
+    private static async Task<AuaPlaydataContent[]> GetPlaydata(HttpClient client, string songname,
+        AuaSongQueryType queryType, ArcaeaDifficulty difficulty,
+        int? startInt, int? endInt, double? startDouble, double? endDouble)
+    {
+        var qb = new QueryBuilder()
+            .Add(queryType == AuaSongQueryType.SongId ? "songid" : "songname", songname)
+            .Add("difficulty", ((int)difficulty).ToString())
+            .Add("start", (startDouble is null
+                ? startInt.ToString()
+                : ((int)Math.Round(startDouble.Value * 100)).ToString())!)
+            .Add("end", (endDouble is null
+                ? endInt.ToString()
+                : ((int)Math.Round(endDouble.Value * 100)).ToString())!);
+        
+        var response = JsonSerializer.Deserialize<AuaResponse<AuaPlaydataContent[]>>(
+            await client.GetStringAsync("playdata" + qb.Build()))!;
+        if (response.Status < 0)
+            throw new AuaException(response.Status, response.Message!);
+        return response.Content!;
+    }
+    
+    /// <summary>
+    /// Get global play data of a song.
+    /// </summary>
+    /// <endpoint>/data/playdata</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
+    /// <param name="queryType">Specify the query type between songname and songid</param>
+    /// <param name="difficulty">Song difficulty</param>
+    /// <param name="start">Range of potential start (100*)</param>
+    /// <param name="end">Range of potential end (100*)</param>
+    /// <returns>Play data content</returns>
+    public async Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
+        ArcaeaDifficulty difficulty, int start, int end)
+        => await GetPlaydata(_client, songname, queryType, difficulty, start, end, null, null);
+
+    /// <summary>
+    /// Get global play data of a song.
+    /// </summary>
+    /// <endpoint>/data/playdata</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
+    /// <param name="queryType">Specify the query type between songname and songid</param>
+    /// <param name="difficulty">Song difficulty</param>
+    /// <param name="start">Range of potential start (100*)</param>
+    /// <param name="end">Range of potential end (100*)</param>
+    /// <returns>Play data content</returns>
+    public async Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
+        ArcaeaDifficulty difficulty, double start, double end)
+        => await GetPlaydata(_client, songname, queryType, difficulty, null, null, start, end);
+
+    /// <summary>
+    /// Get global play data of a song.
+    /// </summary>
+    /// <endpoint>/data/playdata</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
+    /// <param name="queryType">Specify the query type between songname and songid</param>
+    /// <param name="start">Range of potential start (100*)</param>
+    /// <param name="end">Range of potential end (100*)</param>
+    /// <returns>Play data content</returns>
+    public async Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
+        int start, int end)
+        => await GetPlaydata(_client, songname, queryType, ArcaeaDifficulty.Future, start, end, null,
+            null);
+
+    /// <summary>
+    /// Get global play data of a song.
+    /// </summary>
+    /// <endpoint>/data/playdata</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying or sid in Arcaea songlist</param>
+    /// <param name="queryType">Specify the query type between songname and songid</param>
+    /// <param name="start">Range of potential start (100*)</param>
+    /// <param name="end">Range of potential end (100*)</param>
+    /// <returns>Play data content</returns>
+    public async Task<AuaPlaydataContent[]> Playdata(string songname, AuaSongQueryType queryType,
+        double start, double end)
+        => await GetPlaydata(_client, songname, queryType, ArcaeaDifficulty.Future, null, null, start,
+            end);
+
+    /// <summary>
+    /// Get global play data of a song.
+    /// </summary>
+    /// <endpoint>/data/playdata</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying</param>
+    /// <param name="start">Range of potential start (100*)</param>
+    /// <param name="end">Range of potential end (100*)</param>
+    /// <returns>Play data content</returns>
+    public async Task<AuaPlaydataContent[]> Playdata(string songname, int start, int end)
+        => await GetPlaydata(_client, songname, AuaSongQueryType.SongName, ArcaeaDifficulty.Future,
+            start, end, null, null);
+
+    /// <summary>
+    /// Get global play data of a song.
+    /// </summary>
+    /// <endpoint>/data/playdata</endpoint>
+    /// <param name="songname">Any song name for fuzzy querying</param>
+    /// <param name="start">Range of potential start (100*)</param>
+    /// <param name="end">Range of potential end (100*)</param>
+    /// <returns>Play data content</returns>
+    public async Task<AuaPlaydataContent[]> Playdata(string songname, double start, double end)
+        => await GetPlaydata(_client, songname, AuaSongQueryType.SongName, ArcaeaDifficulty.Future,
+            null, null, start, end);
 }
